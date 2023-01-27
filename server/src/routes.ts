1b993d41750d24692896cb339a24658cbb5baf65
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-01-27 17:12:34 -0400

    routes initial

---
+++ server/src/routes.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/habits", async (req) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(req.body);
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map((weekDays) => {
+            return {
+              week_day: weekDays,
+            };
+          }),
+        },
+      },
+    });
+  });
+}

0ec174852a201bd42728a811dab838623017a444
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-01-30 17:30:14 -0400

    summary table, habitday, utils

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,31 @@
+import { HabitDay } from "./HabitDay";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="grid grid-cols-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+      </div>
+    </div>
+  );
+}

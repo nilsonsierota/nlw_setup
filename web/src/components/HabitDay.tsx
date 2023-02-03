618563447003f1fc8f8a6030bb63907fb43732d3
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-02-03 09:01:44 -0400

    fix: Summary table color when zero

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -35,7 +35,7 @@ export function HabitDay({
         className={clsx("w-10 h-10 border-2 rounded-lg", {
           "bg-zinc-900 border-zinc-800": completedPercentage === 0,
           "bg-violet-900 border-violet-700":
-            completedPercentage >= 0 && completedPercentage < 20,
+            completedPercentage > 0 && completedPercentage < 20,
           "bg-violet-800 border-violet-600":
             completedPercentage >= 20 && completedPercentage < 40,
           "bg-violet-700 border-violet-600":
